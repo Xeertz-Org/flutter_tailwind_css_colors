e750bcab94e7d5d9b2bc2afdbde8d2115e0a970f
mrpurpleknight <ziogaghi99@gmail.com>
2024-03-04 15:23:36 +0100

    initial commit

---
+++ example/flutter_tailwind_colors_example.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_tailwind_colors/flutter_tailwind_colors.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/flutter_tailwind_colors.dart
@@ -0,0 +1,3 @@
+library flutter_tailwind_colors;
+
+export 'src/flutter_tailwind_colors_base.dart';
